fbcbc6bd680b25c78f7f6f79731ded9e62f43e51
NicolasFlamel1 <nicolasflamel@mwcwallet.com>
2021-08-07 22:18:06 -0700

    Including build app

--- src/get_mqs_public_key.c
+++ src/get_mqs_public_key.c
@@ -24,7 +24,7 @@ void processGetMqsPublicKeyRequest(unsigned short *responseLength, unsigned char
 	uint8_t *data = &G_io_apdu_buffer[APDU_OFF_DATA];
 
 	// Check if parameters or data are invalid
-	if(firstParameter > TESTNET_NETWORK_TYPE || secondParameter || dataLength <= sizeof(uint32_t)) {
+	if(firstParameter > TESTNET_NETWORK_TYPE || secondParameter || dataLength != sizeof(uint32_t)) {
 	
 		// Throw invalid parameters error
 		THROW(INVALID_PARAMETERS_ERROR);
