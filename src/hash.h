efdfca6c4ee0f831c88904c9a53a165770675dcf
NicolasFlamel1 <nicolasflamel@mwcwallet.com>
2021-11-06 16:52:05 -0700

    Replaced libsecp256k1 hash functions with the Ledger SDK hash functions

--- src/hash.h
+++ src/hash.h
@@ -1,41 +1,73 @@
-/**********************************************************************
- * Copyright (c) 2014 Pieter Wuille                                   *
- * Distributed under the MIT software license, see the accompanying   *
- * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
- **********************************************************************/
+// Header guard
+#ifndef HASH_H
+#define HASH_H
 
-#ifndef SECP256K1_HASH_H
-#define SECP256K1_HASH_H
 
-#include <stdlib.h>
-#include <stdint.h>
+// Header files
+#include <stdbool.h>
+#include "device.h"
 
+
+// Definitions
+
+// RFC6979 V size
+#define RFC6979_V_SIZE 32
+
+// RFC6979 K size
+#define RFC6979_K_SIZE 32
+
+// Secp256k1 SHA256
+typedef cx_sha256_t secp256k1_sha256;
+
+// Secp256k1 HMAC SHA256
+typedef cx_hmac_sha256_t secp256k1_hmac_sha256;
+
+
+// Structures
+
+// Secp256k1 RFC6979 HMAC SHA256
 typedef struct {
-    uint32_t s[8];
-    uint32_t buf[16]; /* In big endian */
-    size_t bytes;
-} secp256k1_sha256;
 
-static void secp256k1_sha256_initialize(secp256k1_sha256 *hash);
-static void secp256k1_sha256_write(secp256k1_sha256 *hash, const unsigned char *data, size_t size);
-static void secp256k1_sha256_finalize(secp256k1_sha256 *hash, unsigned char *out32);
-
-typedef struct {
-    secp256k1_sha256 inner, outer;
-} secp256k1_hmac_sha256;
-
-static void secp256k1_hmac_sha256_initialize(secp256k1_hmac_sha256 *hash, const unsigned char *key, size_t size);
-static void secp256k1_hmac_sha256_write(secp256k1_hmac_sha256 *hash, const unsigned char *data, size_t size);
-static void secp256k1_hmac_sha256_finalize(secp256k1_hmac_sha256 *hash, unsigned char *out32);
-
-typedef struct {
-    unsigned char v[32];
-    unsigned char k[32];
-    int retry;
+	// V
+	uint8_t v[RFC6979_V_SIZE];
+	
+	// K
+	uint8_t k[RFC6979_K_SIZE];
+	
+	// Retry
+	bool retry;
+	
 } secp256k1_rfc6979_hmac_sha256;
 
-static void secp256k1_rfc6979_hmac_sha256_initialize(secp256k1_rfc6979_hmac_sha256 *rng, const unsigned char *key, size_t keylen);
-static void secp256k1_rfc6979_hmac_sha256_generate(secp256k1_rfc6979_hmac_sha256 *rng, unsigned char *out, size_t outlen);
-static void secp256k1_rfc6979_hmac_sha256_finalize(secp256k1_rfc6979_hmac_sha256 *rng);
 
-#endif /* SECP256K1_HASH_H */
+// Function prototypes
+
+// Secp256k1 SHA256 initialize
+static void secp256k1_sha256_initialize(secp256k1_sha256 *hash);
+
+// Secp256k1 SHA256 write
+static void secp256k1_sha256_write(secp256k1_sha256 *hash, const unsigned char *data, size_t length);
+
+// Secp256k1 SHA256 finalize
+static void secp256k1_sha256_finalize(secp256k1_sha256 *hash, unsigned char *out);
+
+// Secp256k1 HMAC SHA256 initialize
+static void secp256k1_hmac_sha256_initialize(secp256k1_hmac_sha256 *hash, const unsigned char *key, size_t length);
+
+// Secp256k1 HMAC SHA256 write
+static void secp256k1_hmac_sha256_write(secp256k1_hmac_sha256 *hash, const unsigned char *data, size_t length);
+
+// Secp256k1 HMAC SHA256 finalize
+static void secp256k1_hmac_sha256_finalize(secp256k1_hmac_sha256 *hash, unsigned char *out);
+
+// Secp256k1 RFC6979 HMAC SHA256 initialize
+static void secp256k1_rfc6979_hmac_sha256_initialize(secp256k1_rfc6979_hmac_sha256 *state, const unsigned char *key, size_t length);
+
+// Secp256k1 RFC6979 HMAC SHA256 generate
+static void secp256k1_rfc6979_hmac_sha256_generate(secp256k1_rfc6979_hmac_sha256 *state, unsigned char *out, size_t length);
+
+// Secp256k1 RFC6979 HMAC SHA256 finalize
+static void secp256k1_rfc6979_hmac_sha256_finalize(secp256k1_rfc6979_hmac_sha256 *state);
+
+
+#endif
