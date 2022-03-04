74d641e298e2c735a54e74bb367e42bfb7f2f027
NicolasFlamel1 <nicolasflamel@mwcwallet.com>
2022-03-03 18:50:15 -0800

    Testing to see if unit tests fail

--- tests/unit_tests/test_bech32.c
+++ tests/unit_tests/test_bech32.c
@@ -6,10 +6,25 @@
 #include "bech32.h"
 
 
+// Constants
+
+// Input
+const uint8_t INPUT[] = {0x00, 0x14, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54, 0x94, 0x1c, 0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6};
+
+// Human-readable part
+const char *HUMAN_READABLE_PART = "hrp";
+
+// Output
+const char OUTPUT[] = "hrp1qq2828nkaqver9k52j2pc3w3kw3j8u2r80tqyrjea";
+
+
 // Function prototypes
 
-// Test Bech32
-static void testBech32(void **state);
+// Test encode
+static void testEncode(void **state);
+
+// Test decode
+static void testDecode(void **state);
 
 
 // Main function
@@ -18,8 +33,11 @@ int main(void) {
 	// Initialize tests
 	const struct CMUnitTest tests[] = {
 	
-		// Test Bech32
-		cmocka_unit_test(testBech32)
+		// Test encode
+		cmocka_unit_test(testEncode),
+		
+		// Test decode
+		cmocka_unit_test(testDecode)
 	};
 	
 	// Return performing tests
@@ -29,38 +47,37 @@ int main(void) {
 
 // Supporting function implementation
 
-// Test Bech32
-void testBech32(void **state) {
+// Test Encode
+void testEncode(void **state) {
 
-	// Constants
-	const uint8_t INPUT[] = {0x00, 0x14, 0x75, 0x1e, 0x76, 0xe8, 0x19, 0x91, 0x96, 0xd4, 0x54, 0x94, 0x1c, 0x45, 0xd1, 0xb3, 0xa3, 0x23, 0xf1, 0x43, 0x3b, 0xd6};
-	const char *HUMAN_READABLE_PART = "hrp";
-	const char OUTPUT[] = "hrp1qq2828nkaqver9k52j2pc3w3kw3j8u2r80tqyrjeaq";
-	
 	// Get output length
 	const size_t outputLength = getBech32EncodedLength(sizeof(INPUT), HUMAN_READABLE_PART);
 	
-	// Assert output length is valid
+	// Assert output length is correct
 	assert_int_equal(outputLength, sizeof(OUTPUT) - sizeof((char)'\0'));
 	
-	// Get output
+	// Get output by encoding input
 	char output[outputLength + sizeof((char)'\0')];
 	bech32Encode(output, INPUT, sizeof(INPUT), HUMAN_READABLE_PART);
 	output[outputLength] = '\0';
 	
-	// Assert output is valid
+	// Assert output is correct
 	assert_string_equal(output, OUTPUT);
-	
+}
+
+// Test Decode
+void testDecode(void **state) {
+
 	// Get input length
 	const size_t inputLength = getBech32DecodedLength(OUTPUT, sizeof(OUTPUT) - sizeof((char)'\0'));
 	
-	// Assert input length is valid
+	// Assert input length is correct
 	assert_int_equal(inputLength, sizeof(INPUT));
 	
-	// Get input
+	// Get input by decoding output
 	uint8_t input[inputLength];
 	bech32Decode(input, OUTPUT, sizeof(OUTPUT) - sizeof((char)'\0'));
 	
-	// Assert input is valid
+	// Assert input is correct
 	assert_memory_equal(input, INPUT, sizeof(INPUT));
 }
